2c893d9349f2d57baf0a0155d92b816b3b4a8098
artogahr <ertogrulgehremanli@gmail.com>
2019-12-18 14:34:58 +0300

    restructuring

--- database/DDL.sql
+++ database/DDL.sql
@@ -2,40 +2,60 @@ DROP DATABASE socialnetwork;
 CREATE DATABASE socialnetwork;
 
 CREATE TABLE users (
-user_id             INT NOT NULL AUTO_INCREMENT,
-user_firstname      VARCHAR(20) NOT NULL,  
-user_lastname       VARCHAR(20) NOT NULL,
-user_nickname       VARCHAR(20),
-user_password       VARCHAR(255) NOT NULL,
-user_email          VARCHAR(255) NOT NULL,
-user_gender         CHAR(1) NOT NULL,
-user_birthdate      DATE NOT NULL,    
-user_status         CHAR(1),
-user_about          TEXT,
-user_hometown       VARCHAR(255),
-PRIMARY KEY (user_id)
+	nationalID int(11) not null,
+	id int(6) not null auto_increment;
+	nameSurname varchar(40);
+	phone int(11),
+	mail varchar(40),
+	password varchar(20), 
+	currentEmployer varchar(40),
+	position varchar(40),
+	workSector varchar(40),
+	bachelorUni varchar(40),
+	masterUni varchar(40),
+	doctorateUni varchar(40),
+	foreignLanguage varchar(40),
+	certificate varchar(40),
+	pastWorkID int(11),
+	userType varchar(7),
+	graduationDate date,
+	gender varchar(6)
+	PRIMARY KEY (id)
 );
 
+CREATE TABLE companies (
+	companyName varchar(40) not null auto_increment,
+	phone int(11),
+	mail varchar(40),
+	adress varchar(50),
+	workSector varchar(15)
+)
+
 CREATE TABLE friendship (
-user1_id            INT NOT NULL,
-user2_id            INT NOT NULL,
-friendship_status   INT NOT NULL,
-FOREIGN KEY (user1_id) REFERENCES users(user_id),
-FOREIGN KEY (user2_id) REFERENCES users(user_id)
+	user1_id            INT NOT NULL,
+	user2_id            INT NOT NULL,
+	friendship_status   INT NOT NULL,
+	FOREIGN KEY (user1_id) REFERENCES users(user_id),
+	FOREIGN KEY (user2_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE posts (
-id             INT NOT NULL AUTO_INCREMENT,
-caption        TEXT NOT NULL,
-time           TIMESTAMP NOT NULL, 
-public         CHAR(1) NOT NULL,
-user             INT NOT NULL,
-PRIMARY KEY (id),
-FOREIGN KEY (user) REFERENCES students(user_id)
+	id             INT NOT NULL AUTO_INCREMENT,
+	time           date NOT NULL, 
+	user             INT NOT NULL,
+	companyName	varchar(50),
+	public         CHAR(1) NOT NULL,
+	announcementType varchar(10),
+	deadline date not null,
+	internshipTerm varchar(10),
+	startDate date not null,
+	endDate date not null,
+	PRIMARY KEY (id),
+	FOREIGN KEY (user) REFERENCES students(id)
 );
 
 CREATE TABLE user_phone (
-user_id         INT,
-user_phone      INT,
-FOREIGN KEY (user_id) REFERENCES users(user_id)
+	user_id         INT,
+	user_phone      INT,
+	FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
